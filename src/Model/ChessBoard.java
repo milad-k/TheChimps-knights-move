fbbfc4e512fd111205f566a6a55410239e7dd562
maryam626 <maryam_khalil_7@outlook.com>
2022-12-11 12:38:24 +0200

    ..

--- src/Model/ChessBoard.java
+++ src/Model/ChessBoard.java
@@ -105,6 +105,7 @@ public class ChessBoard {
             Random rand = new Random();
             int int_rand = rand.nextInt(64);
             squares.get(int_rand).setType("Random Jump Square");
+            //squares.get(int_rand).setBackground(new Background(new BackgroundFill(color1, CornerRadii.EMPTY, Insets.EMPTY)));
 
         }
     }
