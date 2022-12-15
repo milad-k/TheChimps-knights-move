d0f70cdb9f3318db8d649d26ef2fc65675a01a44
maryam626 <maryam_khalil_7@outlook.com>
2022-12-16 00:56:42 +0200

    overriding methods from chessboard class

--- src/Model/ChessBoard.java
+++ src/Model/ChessBoard.java
@@ -22,24 +22,9 @@ public abstract class ChessBoard {
         makeBoard(this.chessBoard, theme);
     }
 
-    private void makeBoard(GridPane chessBoard, String theme) {
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                Square square = new Square(i, j);
-                square.setName("Square" + i + j);
-                square.setPrefHeight(100);
-                square.setPrefWidth(100);
-                square.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
-                setTheme(square, theme, i, j);
-                chessBoard.add(square, i, j, 1, 1);
-                squares.add(square);
-            }
-        }
-        addPieces();
-        addSpecialSquares();
-    }
+    abstract void makeBoard(GridPane chessBoard, String theme);
 
-    private void setTheme(Square square, String theme, int i, int j) {
+    public void setTheme(Square square, String theme, int i, int j) {
         Color color1 = Color.web("#ffffff00");
         Color color2 = Color.web("#ffffff00");
 
@@ -82,32 +67,13 @@ public abstract class ChessBoard {
         square.occupied = true;
     }
 
-    private void addPieces() {
-        for (Square square : squares) {
-            if (square.occupied)
-                continue;
-            if (square.y == 0) {
-                /*if(square.x == 7){
-                    addPiece(square, new King("white", square.x, square.y));
-                }*/
-                if (square.x == 7) {
-                    addPiece(square, new Queen("black", square.x, square.y));
-                }
-                if (square.x == 0) {
-                    addPiece(square, new Knight("white", square.x, square.y));
-                }
-            }
-        }
-    }
+    abstract void addPieces();
 
 
 
 
     public ArrayList<Square> getSquares() {
         return squares;
-    }
-    public void addSpecialSquares(){
-
     }
     public void setSquares(ArrayList<Square> squares) {
         this.squares = squares;
--- src/Model/StageOneChessBoard.java
+++ src/Model/StageOneChessBoard.java
@@ -1,7 +1,7 @@
 package Model;
 
 import Utils.Stage;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 
 import java.util.Random;
@@ -15,6 +15,39 @@ public class StageOneChessBoard extends ChessBoard{
 
     }
 
+    @Override
+     void makeBoard(GridPane chessBoard, String theme){
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                Square square = new Square(i, j);
+                square.setName("Square" + i + j);
+                square.setPrefHeight(100);
+                square.setPrefWidth(100);
+                square.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+                setTheme(square, theme, i, j);
+                chessBoard.add(square, i, j, 1, 1);
+                squares.add(square);
+            }
+        }
+        addPieces();
+        addRandomJumpSquares();
+    }
+
+    @Override
+    void addPieces(){
+        for (Square square : squares) {
+            if (square.occupied)
+                continue;
+            if (square.y == 0) {
+                if (square.x == 7) {
+                    addPiece(square, new Queen("black", square.x, square.y));
+                }
+                if (square.x == 0) {
+                    addPiece(square, new Knight("white", square.x, square.y));
+                }
+            }
+        }
+    }
     private void addRandomJumpSquares() {
 
         Color color1 = Color.web("black");
