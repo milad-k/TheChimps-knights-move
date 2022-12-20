0fb9d3b5334753f89d94c264c2394f99e01225fe
maryam626 <maryam_khalil_7@outlook.com>
2022-12-20 14:37:14 +0200

    updating tests and put one assertion

--- src/JUnit/LoadQuestionTest.java
+++ src/JUnit/LoadQuestionTest.java
@@ -6,7 +6,6 @@ import Utils.Difficulty;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 
 import static org.junit.Assert.assertTrue;
@@ -17,8 +16,6 @@ public class LoadQuestionTest{
     public void test() {
         SysData.getInstance().loadQuestions(null);
         HashMap<Difficulty, ArrayList<Question>> questions = SysData.getInstance().getQuestions();
-        //SysData.getInstance().saveQuestions(null);
-
         SysData.getInstance().loadQuestions(null);
         HashMap<Difficulty, ArrayList<Question>> questions2 = SysData.getInstance().getQuestions();
 
