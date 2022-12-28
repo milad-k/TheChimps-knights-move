86bf2229b37c8c47247031e1bdbdfd924f087459
maryam626 <maryam_khalil_7@outlook.com>
2022-12-28 23:26:22 +0200

    modifying cup screen controller

--- src/Controller/CupScreenController.java
+++ src/Controller/CupScreenController.java
@@ -1,5 +1,65 @@
 package Controller;
 
-public class CupScreenController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
+import java.io.IOException;
+
+public class CupScreenController {
+    @FXML
+    private Button playagainButton;
+
+    @FXML
+    private Button homeButton;
+
+    public void home(ActionEvent actionEvent) {
+        Stage stage = (Stage) homeButton.getScene().getWindow();
+        stage.close();
+        Stage stage1 = (Stage) Window.getWindows().get(0).getScene().getWindow();
+        stage1.close();
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("../View/HomeScreen.fxml"));
+            root.setStyle("-fx-background-image: url('Images/backgroundWallpaper.jpeg');" + "-fx-background-size:cover");
+            Scene customerScene = new Scene(root);
+            Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            window.setScene(customerScene);
+            window.show();
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("FXML");
+            alert.setHeaderText("Load failure");
+            alert.setContentText("Failed to load the FXML file.");
+            alert.showAndWait();
+        }
+    }
+
+    public void playAgain(ActionEvent actionEvent) {
+        Stage stage = (Stage) playagainButton.getScene().getWindow();
+        stage.close();
+        Stage stage1 = (Stage) Window.getWindows().get(0).getScene().getWindow();
+        stage1.close();
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("../View/Game.fxml"));
+            root.setStyle("-fx-background-image: url('Images/backgroundWallpaper.jpeg');" + "-fx-background-size:cover");
+            Scene customerScene = new Scene(root);
+            Stage window = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            window.setScene(customerScene);
+            window.show();
+        } catch (IOException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("FXML");
+            alert.setHeaderText("Load failure");
+            alert.setContentText("Failed to load the FXML file.");
+            alert.showAndWait();
+        }
+
+    }
 }
