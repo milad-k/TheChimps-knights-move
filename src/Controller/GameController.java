c5fefaf4b41af3c41cdad4a8ead45bf06b9c1140
Milad Kittan <miladkittan1@gmail.com>
2022-12-28 19:00:17 +0200

    delete comments

--- src/Controller/GameController.java
+++ src/Controller/GameController.java
@@ -93,21 +93,6 @@ public class GameController {
             alert.setContentText("Failed to load the FXML file.");
             alert.showAndWait();
         }
-
-        /*try {
-            Parent root = FXMLLoader.load(getClass().getResource("../View/HomeScreen.fxml"));
-            root.setStyle("-fx-background-image: url('Images/backgroundWallpaper.jpeg');" + "-fx-background-size:cover");
-            Scene customerScene = new Scene(root);
-            Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            window.setScene(customerScene);
-            window.show();
-        } catch (IOException e) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("FXML");
-            alert.setHeaderText("Load failure");
-            alert.setContentText("Failed to load the FXML file.");
-            alert.showAndWait();
-        }*/
     }
     @FXML
     void pause(ActionEvent event) throws IOException {
