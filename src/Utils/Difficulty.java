20e0f88169ce81aee29f5f83ae317eab262cd67f
maryam626 <maryam_khalil_7@outlook.com>
2022-12-30 22:08:06 +0200

    putting winning and loosing points functions to Difficulty class

--- src/Utils/Difficulty.java
+++ src/Utils/Difficulty.java
@@ -1,5 +1,7 @@
 package Utils;
 
+import Model.Question;
+
 import java.util.Random;
 
 public enum Difficulty {
@@ -13,6 +15,28 @@ public enum Difficulty {
         Difficulty[] difficulties = values();
         int int_rand = PRNG.nextInt(3);
         return difficulties[int_rand];
+    }
+    public static int gettingWinningPoints(Question q){
+        if(q.getLevel().equals(Difficulty.EASY)){
+            return 1;
+        }
+        else if(q.getLevel().equals(Difficulty.MEDIUM)){
+            return 2;
+        }
+        else{
+            return 3;
+        }
+    }
+    public static int gettingLoosingPoints(Question q){
+        if(q.getLevel().equals(Difficulty.EASY)){
+            return -2;
+        }
+        else if(q.getLevel().equals(Difficulty.MEDIUM)){
+            return -3;
+        }
+        else{
+            return -4;
+        }
     }
 
 }
