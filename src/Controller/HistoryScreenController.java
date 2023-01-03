1725eeed89139521934244449f9fda4002c3ddb3
maramkhoury <maram.khoury98@gmail.com>
2023-01-03 09:14:07 +0200

    history controller

--- src/Controller/HistoryScreenController.java
+++ src/Controller/HistoryScreenController.java
@@ -1,5 +1,9 @@
 package Controller;
 
+import Model.SysData;
+import Model.User;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -7,11 +11,36 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
 public class HistoryScreenController {
+
+    @FXML
+    private TableColumn<User, String> avatarHistory;
+
+    @FXML
+    private Button backButton1;
+
+    @FXML
+    private TableView<User> historyTable;
+
+    @FXML
+    private TableColumn<User, Integer> scoreHistory;
+
+    @FXML
+    private TableColumn<User, String> userNameHistory;
+
+    private ArrayList<User> Players;
+
     @FXML
     void back(ActionEvent event) {
         try {
@@ -28,6 +57,20 @@ public class HistoryScreenController {
             alert.setContentText("Failed to load the FXML file.");
             alert.showAndWait();
         }
+    }
+
+    public void initialize(URL location, ResourceBundle resources) {
+        userNameHistory.setCellValueFactory(new PropertyValueFactory<>("nickname"));
+        avatarHistory.setCellValueFactory(new PropertyValueFactory<>("avatar"));
+        scoreHistory.setCellValueFactory(new PropertyValueFactory<>("GameHighScore"));
+        setHistoryTable();
+    }
+
+    private void setHistoryTable() {
+        Players = SysData.getInstance().getUsers();
+        ObservableList<User> qs = FXCollections.observableArrayList(Players);
+        historyTable.setItems(qs);
+        historyTable.refresh();
     }
 
 
